648b4bcb1a1380a3728f1217866b1e8902557764
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-09-29 19:26:39 -0700

    Improve test to verify injection field name

--- src/test/java/com/fasterxml/jackson/databind/deser/inject/JacksonInject3072Test.java
+++ src/test/java/com/fasterxml/jackson/databind/deser/inject/JacksonInject3072Test.java
@@ -76,7 +76,7 @@ class JacksonInject3072Test extends DatabindTestUtil
                 MissingInjectableValueExcepion.class, () -> reader.readValue("{}"));
 
         assertThat(exception.getMessage())
-            .startsWith("No injectable value with id 'requiredValue' found (for property ");
+            .startsWith("No injectable value with id 'requiredValue' found (for property 'requiredField')");
 
         // Also check the other code path, with non-null Injectables
         ObjectReader reader2 = reader.with(new InjectableValues.Std()
